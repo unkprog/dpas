979b31bcf6525067994453c0463da251355d1ebe
unkprog <unkprog@gmail.com>
2016-09-22 21:18:51 +0300

    1. Добавлен консольный лог 2. Добавлен старт и стоп сервера 3. Исправлена утечка в случае ошибки

---
+++ dpas.Core/IO/Debug/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dpas.Core.IO.Debug
+{
+    public interface ILog
+    {
+        void Write(string data);
+    }
+
+    public class LogBase : ILog
+    {
+        internal static ILog log;
+
+        public static void WriteToLog(string data)
+        {
+            if (log != null)
+                log.Write(data);
+        }
+
+        public void Write(string data)
+        {
+            if (log != null)
+                log.Write(data);
+        }
+    }
+
+   
+
+    public class LogConsole : ILog
+    {
+        public void Write(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        public static void Setup()
+        {
+            LogBase.log = new LogConsole();
+        }
+    }
+
+}
