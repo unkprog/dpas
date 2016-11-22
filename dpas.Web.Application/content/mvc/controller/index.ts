809cdb7adeb05ac11882edabeed75770f83f6cfa
unkprog <unkprog@gmail.com>
2016-11-22 09:48:28 +0300

    В качестве GUI добавлена materializecss

--- dpas.Web.Application/content/mvc/controller/index.ts
+++ dpas.Web.Application/content/mvc/controller/index.ts
@@ -1,7 +1,7 @@
 ﻿declare var $: any;
 export module View {
     export class Index {
-
+        
         //public ShowCarousel() {
         //    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         //   // $('#modal1').openModal();
