32444ffdfb601af70eb9e3f7aa2936b5e63ab702
unkprog <unkprog@gmail.com>
2016-12-08 10:05:53 +0300

    Доработан концепт внешнего вида редактора проекта

--- dpas.Web.Application/content/mvc/controller/prj/editor.js
+++ dpas.Web.Application/content/mvc/controller/prj/editor.js
@@ -13,6 +13,23 @@ var View;
                 });
                 $(".tree").treemenu({ delay: 300 }); //.openActive();
                 $('ul.tabs').tabs();
+                that.ApplyLayout();
+                $(window).resize(function () {
+                    that.ApplyLayout();
+                });
+            };
+            Editor.prototype.ApplyLayout = function () {
+                var h = window.innerHeight - $('.navbar-fixed').height();
+                $('#editor-menu').height(h);
+                $('#editor-menu-tree').height(h - 64);
+                $('#editor-content').height(h);
+                h = h - $('#editor-tabs').height();
+                $('#editor-designer-view').height(h);
+                $('#editor-code-view').height(h);
+                $('#code-view-textarea').height(h);
+                var w = window.innerWidth - $('#editor-menu').width();
+                $('#editor-content').width(w);
+                $('#code-view-textarea').width(w - 4);
             };
             return Editor;
         }());
--- dpas.Web.Application/content/mvc/controller/prj/editor.ts
+++ dpas.Web.Application/content/mvc/controller/prj/editor.ts
@@ -14,6 +14,27 @@ export module View {
 
                 $(".tree").treemenu({ delay: 300 }); //.openActive();
                 $('ul.tabs').tabs();
+                that.ApplyLayout();
+                
+                $(window).resize(function () {
+                    that.ApplyLayout();
+                });
+                
+            }
+
+            ApplyLayout() {
+                var h = window.innerHeight - $('.navbar-fixed').height();
+                $('#editor-menu').height(h);
+                $('#editor-menu-tree').height(h - 64);
+                $('#editor-content').height(h);
+                h = h - $('#editor-tabs').height();
+                $('#editor-designer-view').height(h);
+                $('#editor-code-view').height(h);
+                $('#code-view-textarea').height(h);
+
+                var w = window.innerWidth - $('#editor-menu').width();
+                $('#editor-content').width(w);
+                $('#code-view-textarea').width(w - 4);
             }
         }
     }
